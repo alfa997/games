738671d0c8628be9e845562a8efaa137a323019e
alfa997 <daffaalfaro81@gmail.com>
2026-02-15 20:21:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-function appendToDisplay(value) {
-    const display = document.getElementById('display');
-    display.value += value;
-}
 
-function calculate() {
-    const display = document.getElementById('display');
-    const expression = display.value;
-    
-    // This will replace the result of any mathematical operation with "I love you"
-    if (expression.match(/[0-9]+[+\-*/][0-9]+/)) {
-        display.value = 'I love you kagak canda nghehe';
-    } else {
-        try {
-            display.value = eval(expression);
-        } catch {
-            display.value = 'Error';
-        }
-    }
-}
 
-function clearDisplay() {
-    const display = document.getElementById('display');
-    display.value = '';
+
